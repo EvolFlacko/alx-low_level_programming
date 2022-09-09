c9c2e8cf1699cd0d24a9201fd67f11135f28cf28
Adekunle Emmanuel <xedagold@gmail.com>
2022-09-09 16:43:28 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <studio.h>
+
 /**
- **main- Serve as the entry point for the program
+ * *main - Entry point
  * *
- *  *Return: By default returns zero if no error otherwise returns non-zero value
- *   */
+ * *Return: Always 0
+ * */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
