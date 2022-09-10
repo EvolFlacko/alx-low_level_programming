8dd6a365a1e3f1660bb8fcfc4d5496d696ea197c
Adekunle Emmanuel <xedagold@gmail.com>
2022-09-10 06:21:11 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,27 @@
-#include <studio.h>
+#include <stdio.h>
+
+
 
 /**
- * *main - Entry point
- * *
- * *Return: Always 0
- * */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+ *
+ *  * main - Prints "Programming is like building a multilingual
+ *
+ *   *                puzzle, followed by a new line.
+ *
+ *    *
+ *
+ *     * Return: Always 0.
+ *
+ *      */
 
-	return (0);
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
 }
